bd29cb7a66a594c76d6e45354abf5335ea49d2c3
N. Venkat Raj <venkat@codinggeek.com>
2022-07-11 11:04:35 +0530

    required methods

--- src/Options.php
+++ src/Options.php
@@ -2,5 +2,43 @@
 if (!defined('ABSPATH')) die('No direct access allowed');
 
 class Options {
+	public function __construct() {
+		
+	}
 
+	public function get() {
+
+	}
+
+	public function getAllOptions() {
+
+	}
+
+	public function getDefaultOptions() {
+
+	}
+
+	public function setDefaultOptions() {
+		
+	}
+
+	public function update() {
+
+	}
+
+	public function delete() {
+
+	}
+
+	public function reset() {
+
+	}
+
+	public function import() {
+
+	}
+
+	public function export() {
+
+	}
 }
